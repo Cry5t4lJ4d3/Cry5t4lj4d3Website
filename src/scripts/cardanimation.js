5a02e58ddf1a684494f964b22de8ae3be13cc83e
cry5t4lj4d3 <fun9usf1sh@outlook.com>
2026-07-04 13:30:23 +0800

    加入了一点名片特效

---
+++ src/scripts/cardanimation.js
@@ -0,0 +1,26 @@
+const cards = document.querySelector('.card');
+
+cards.addEventListener("mousemove", function(e){
+        //鼠标位置计算
+    const l = e.offsetX;
+    const t = e.offsetY;
+    var h = cards.offsetHeight;
+    var w = cards.offsetWidth;
+    var px = Math.abs(Math.floor(100/w*l));
+    var py = Math.abs(Math.floor(100/h*t));
+        //倾斜角度计算
+    var tp = ((px-50)/1.5)
+    var lp = ((py-50)/1.5)
+    var ty = (tp/2)*-1;
+    var tx = (lp/2*.5);
+    //css实现
+    cards.style.transform = `rotateX(${ty}deg) rotateY(${tx}deg)`;
+    //css应用
+    cards.style.setProperty('--mouse-x',`${lp}%`);
+    cards.style.setProperty('--mouse-y',`${tp}%`);
+})
+cards.addEventListener("mouseout",function(){
+    cards.style.transform = "";
+    cards.style.removeProperty('--mouse-x');
+    cards.style.removeProperty('--mouse-y');
+});
