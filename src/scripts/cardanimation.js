25e075f594a65ecb8ebd14ce06ea35c066fa05bf
cry5t4lj4d3 <fun9usf1sh@outlook.com>
2026-07-04 14:22:36 +0800

    优化了一下效果，给加了个背景，现在我的名片很像在吸血鬼幸存者里开出来的装备

--- src/scripts/cardanimation.js
+++ src/scripts/cardanimation.js
@@ -12,7 +12,7 @@ cards.addEventListener("mousemove", function(e){
     var tp = ((px-50)/1.5)
     var lp = ((py-50)/1.5)
     var tx = (tp/2)*-1;
-    var ty = (lp/1.5*.5);
+    var ty = (lp/2*1);
     //css实现
     cards.style.transform = `rotateX(${ty}deg) rotateY(${tx}deg)`;
     //css应用
